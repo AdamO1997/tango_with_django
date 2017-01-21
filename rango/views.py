8bea189e1c69425266015a9d47203f31e27aeaf5
Adam O'Neill <Adam O'Neill>
2017-01-21 14:53:02 +0000

    template started

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    # Construct a dictionary to pass to the tamplate engine as its context.
+    # Note the key boldmessage is the same as {{boldmessage}} in the template!
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make out lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
